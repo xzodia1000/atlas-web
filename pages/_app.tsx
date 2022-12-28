4b7d38cfd2a71574d0e2868df219d8394cd303e6
Nidal <nidalaraf@gmail.com>
2022-12-29 02:19:56 +0400

    Added admin login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { SessionProvider } from '../lib/context';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  });
+
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 }
